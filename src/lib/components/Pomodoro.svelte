749dbb62370d6265da7e8cc2c96fbaeaa4ddbc25
skoshx <rasmus.gustafsson2611@gmail.com>
2021-06-06 21:34:22 +0300

    Added an image to README

--- src/lib/components/Pomodoro.svelte
+++ src/lib/components/Pomodoro.svelte
@@ -117,7 +117,7 @@ import { writable } from "svelte/store";
       {/if}
     </div>
     <div class="w-full focus:outline-none hover:bg-gray-200 transition bg-red-50 px-4 py-2 shadow-xl text-gray-900 font-semibold flex justify-center items-center tracking-normal cursor-pointer" on:click={reset}>
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
       </svg>
       Reset
